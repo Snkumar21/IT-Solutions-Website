7c15672cd68aca92a531112c27d22ee8b303e350
Nitish <snkumar6926@gmail.com>
2025-11-01 00:25:37 +0530

    Frontend design successful

--- backend/models/Services.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ServiceSchema = new mongoose.Schema({
-  title: {type:String, required:true},
-  short: String,
-  description: String,
-  createdAt: {type:Date, default:Date.now}
-});
-module.exports = mongoose.model('Service', ServiceSchema);
